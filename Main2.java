91262a3d41c304357c880691757c58819946107d
thitapornyunin220-lab <thitapornyunin220@gmail.com>
2026-07-24 08:46:04 +0700

    หาพื้นที่วงกลม

---
+++ Main2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        double x = kb.nextDouble();
+        double area = 3.145 * x*x;
+        System.out.println(area);
+    }
+}
